06d9be9ffd2832fdbf3abbf9f4874f1e9f11f2ff
Oskar Ortiz <okyjulian994@gmail.com>
2026-03-17 14:36:10 -0500

    ".next-app"

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  distDir: ".next-app",
+  ...(process.env.VERCEL ? {} : { distDir: ".next-app" }),
 };
 
 export default nextConfig;
